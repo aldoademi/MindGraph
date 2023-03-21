c9b4d3f7a0d45b7ca24abcf950509684675a7b5c
aldoademi <aldo.ademi011299@gmail.com>
2023-03-21 20:27:14 +0100

    Aggiunta interfaccia grafica

--- main.py
+++ main.py
@@ -2,30 +2,54 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import tkinter as tk
+from tkinter import filedialog
 
-# Carica l'immagine
-img = cv2.imread('/home/aldoademi/Desktop/targets/economist_daily_chart_103.png')
+# Funzione per selezionare l'immagine
+def select_image():
+    # Apre la finestra di dialogo per selezionare un file
+    path = filedialog.askopenfilename()
+    if len(path) > 0:
+        # Carica l'immagine
+        img = cv2.imread(path)
+        
+        # Ridimensiona l'immagine a una dimensione fissa
+        img = cv2.resize(img, (224, 224))
+        
+        # Normalizza l'immagine
+        img = img.astype('float32') / 255.0
+        
+        # Crea un modello di rete neurale convoluzionale per calcolare la mappa di salienza
+        model = keras.applications.VGG16(include_top=False, weights=None, input_shape=(224, 224, 3))
+        x = model.output
+        x = keras.layers.Conv2D(1, (3, 3), padding='same', activation='sigmoid')(x)
+        salient_map_model = keras.models.Model(model.input, x)
+        
+        # Calcola la mappa di salienza dell'immagine
+        saliency_map = salient_map_model.predict(np.expand_dims(img, axis=0))[0, :, :, 0]
+        
+        # Applica una soglia alla mappa di salienza per ottenere una maschera binaria
+        _, saliency_mask = cv2.threshold(saliency_map, 0.5, 1, cv2.THRESH_BINARY)
+        
+        # Calcola il rapporto tra il numero di pixel salienti nella maschera e il numero totale di pixel dell'immagine
+        memorability = np.sum(saliency_mask) / (saliency_mask.shape[0] * saliency_mask.shape[1])
+        
+        # Aggiorna l'interfaccia grafica con il grado di memorabilità calcolato
+        result_label.config(text=f"Grado di memorabilità dell'immagine: {memorability:.2f}")
+        
 
-# Ridimensiona l'immagine a una dimensione fissa
-img = cv2.resize(img, (224, 224))
+# Crea l'interfaccia grafica
+root = tk.Tk()
+root.title("Calcolo grado di memorabilità")
+root.geometry("400x200")
 
-# Normalizza l'immagine
-img = img.astype('float32') / 255.0
+# Aggiunge un pulsante per selezionare l'immagine
+select_button = tk.Button(root, text="Seleziona immagine", command=select_image)
+select_button.pack(pady=20)
 
-# Crea un modello di rete neurale convoluzionale per calcolare la mappa di salienza
-model = keras.applications.VGG16(include_top=False, weights=None, input_shape=(224, 224, 3))
-x = model.output
-x = keras.layers.Conv2D(1, (3, 3), padding='same', activation='sigmoid')(x)
-salient_map_model = keras.models.Model(model.input, x)
+# Aggiunge una label per mostrare il risultato
+result_label = tk.Label(root, text="")
+result_label.pack()
 
-# Calcola la mappa di salienza dell'immagine
-saliency_map = salient_map_model.predict(np.expand_dims(img, axis=0))[0, :, :, 0]
-
-# Applica una soglia alla mappa di salienza per ottenere una maschera binaria
-_, saliency_mask = cv2.threshold(saliency_map, 0.5, 1, cv2.THRESH_BINARY)
-
-# Calcola il rapporto tra il numero di pixel salienti nella maschera e il numero totale di pixel dell'immagine
-memorability = np.sum(saliency_mask) / (saliency_mask.shape[0] * saliency_mask.shape[1])
-
-# Stampa il grado di memorabilità
-print("Grado di memorabilità dell'immagine: ", memorability)
+# Avvia l'interfaccia grafica
+root.mainloop()
