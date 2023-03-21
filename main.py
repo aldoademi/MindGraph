2e8bc923bec4d6bcd1b8e09b3b76533084145689
aldoademi <aldo.ademi011299@gmail.com>
2023-03-21 19:46:40 +0100

    Modifiche utilizzando un modello di rete neurale convoluzionale per calcolare la mappa di salienza

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 # Carica l'immagine
 img = cv2.imread('/home/aldoademi/Desktop/targets/economist_daily_chart_103.png')
 
-# Converte l'immagine in formato CV_32FC1 e in scala di grigi
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype('float32') / 255
-img_scaled = img * 255
-img_scaled = img_scaled.astype('uint8')
+# Ridimensiona l'immagine a una dimensione fissa
+img = cv2.resize(img, (224, 224))
 
-# Calcola la mappa dei salienti dell'immagine con il metodo "Spectral Residual"
-saliency_map = cv2.saliency.StaticSaliencySpectralResidual_create().computeSaliency(img)
+# Normalizza l'immagine
+img = img.astype('float32') / 255.0
 
-# Applica una soglia alla mappa dei salienti per ottenere una maschera binaria
-_, saliency_mask = cv2.threshold(img_scaled, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# Crea un modello di rete neurale convoluzionale per calcolare la mappa di salienza
+model = keras.applications.VGG16(include_top=False, weights=None, input_shape=(224, 224, 3))
+x = model.output
+x = keras.layers.Conv2D(1, (3, 3), padding='same', activation='sigmoid')(x)
+salient_map_model = keras.models.Model(model.input, x)
 
-# Calcola il rapporto tra il numero di pixel bianchi nella maschera e il numero totale di pixel dell'immagine
-memorability = cv2.countNonZero(saliency_mask) / (img.shape[0] * img.shape[1])
+# Calcola la mappa di salienza dell'immagine
+saliency_map = salient_map_model.predict(np.expand_dims(img, axis=0))[0, :, :, 0]
+
+# Applica una soglia alla mappa di salienza per ottenere una maschera binaria
+_, saliency_mask = cv2.threshold(saliency_map, 0.5, 1, cv2.THRESH_BINARY)
+
+# Calcola il rapporto tra il numero di pixel salienti nella maschera e il numero totale di pixel dell'immagine
+memorability = np.sum(saliency_mask) / (saliency_mask.shape[0] * saliency_mask.shape[1])
 
 # Stampa il grado di memorabilità
 print("Grado di memorabilità dell'immagine: ", memorability)
